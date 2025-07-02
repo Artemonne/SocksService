fe6dd07b9a79ff02ca81dac3cb5e0162e14b073b
Artemonne <artem_alekseev04@mail.ru>
2025-07-02 22:30:47 +0300

    add user to client layer

--- client/src/app/Layout/Layout.jsx
+++ client/src/app/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router';
 export default function Layout() {
   return (
     <div>
-      Layout
+      {/* Layout */}
       <Outlet />
     </div>
   );
--- client/src/app/Routes/Router.jsx
+++ client/src/app/Routes/Router.jsx
@@ -1,14 +1,29 @@
 import { Routes, Route } from 'react-router';
+import {useEffect, useState, React} from 'react';
+import { UserApi } from '../../entities/user/UserApi.js';
+import { setAccessToken } from '../../shared/lib/axiosInstance.js';
 import Layout from '../Layout/Layout.jsx';
 import BasketPage from '../../Pages/BasketPage';
 import FavouritePage from '../../Pages/FavouritePage.jsx';
+import AuthPage from '../../Pages/AuthPage/AuthPage.jsx';
 
 export default function Router() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    UserApi.refreshTokens().then((serverResponse) => {
+      if (serverResponse.error) return;
+      setUser(serverResponse.data.user);
+      setAccessToken(serverResponse.data.accessToken);
+    });
+  }, []);
+  
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/basket" element={<BasketPage />} />
         <Route path="/favourites" element={<FavouritePage />} />
+        <Route path="/auth" element={<AuthPage setUser={setUser} />} />
       </Route>
     </Routes>
   );
