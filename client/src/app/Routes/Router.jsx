656670b5151d383cc062f97ba92dbf06f37faf93
medvtanya <medvtanya13@gmail.com>
2025-07-02 22:00:01 +0300

    mainPage

--- client/src/app/Routes/Router.jsx
+++ client/src/app/Routes/Router.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router';
 import Layout from '../Layout/Layout.jsx';
 import BasketPage from '../../Pages/BasketPage';
 import FavouritePage from '../../Pages/FavouritePage.jsx';
+import MainPage from '../../Pages/MainPage.jsx';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="/" element={<MainPage />} />
         <Route path="/basket" element={<BasketPage />} />
         <Route path="/favourites" element={<FavouritePage />} />
       </Route>
