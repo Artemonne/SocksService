c0791ba37157e4836a1cf94572ce66937050a557
medvtanya <medvtanya13@gmail.com>
2025-07-02 11:17:54 +0300

    init_server/client

---
+++ client/src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Router from './Routes/Router';
+
+function App() {
+  return (
+    <>
+      <h1>Привет!</h1>
+      <Router />
+    </>
+  );
+}
+
+export default App;
