06a832bc745c8f1cab30c004e8e1d7a691263ec4
bolatchiev <bolatchiev.azamat@bk.ru>
2025-07-02 14:35:47 +0300

    db_done

--- server/db/migrations/3-20250702110621-create-basket.js
+++ server/db/migrations/3-20250702110621-create-basket.js
@@ -10,18 +10,32 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       userId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER, 
+        references: {
+          model:'Users',
+          key:'id',
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE"
       },
       sockId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model:'Socks',
+          key:'id',
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE"
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
       }
     });
   },
--- server/db/models/favourite.js
+++ server/db/models/favourite.js
@@ -1,21 +1,15 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Favourite extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Favourite.belongsTo(models.User, { foreignKey: 'userId' });
+      Favourite.belongsTo(models.Sock, { foreignKey: 'sockId' });
     }
   }
   Favourite.init({
-    userId: DataTypes.INTEGER,
-    sockId: DataTypes.INTEGER
+    userId: DataTypes.BIGINT,
+    sockId: DataTypes.BIGINT
   }, {
     sequelize,
     modelName: 'Favourite',
