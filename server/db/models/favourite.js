c8ab8ec28689fde5bd7c43aaf9dab68fc7c89525
bolatchiev <bolatchiev.azamat@bk.ru>
2025-07-02 15:24:43 +0300

    fix_db_2.0

--- server/db/models/favourite.js
+++ server/db/models/favourite.js
@@ -8,8 +8,8 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Favourite.init({
-    userId: DataTypes.BIGINT,
-    sockId: DataTypes.BIGINT
+    userId: DataTypes.INTEGER,
+    sockId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Favourite',
